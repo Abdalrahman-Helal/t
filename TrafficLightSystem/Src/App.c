9c68348b0c562f56764a49d1aaf4c0d240e9a3f5
Abdalrahman Helal <121490216+Abdalrahman-Helal@users.noreply.github.com>
2024-08-14 23:16:19 +0300

    Add files via upload

--- TrafficLightSystem/Src/App.c
+++ TrafficLightSystem/Src/App.c
@@ -14,61 +14,61 @@
 #include "../Include/HAL/SSD/SSD_Interface.h"
 #include "../Include/HAL/LED/LED_Interface.h"
 
-#define F_CPU 8000000UL
+
+#define F_CPU 12000000UL
 
 int main(void)
 {
+	// Configure PORTA as Output for SSD1
 	MDIO_voidSetPortDirection(DIO_PORTA,0xFF);
+
+	// Configure PORTB as Output for SSD2
+	MDIO_voidSetPortDirection(DIO_PORTB,0xFF);
+
+//	//Configure PORTC as Output for LEDs(Green,Yellow ,Red)
+	MDIO_voidSetPortDirection(DIO_PORTC,0x07);
+
+	u8 SSD_Arr[10]={ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE};
+	u8 LED_Arr[3]={GREEN,YELLOW ,RED};
+	u8 i=3;
+	u8 j=0;
 	while(1)
 	{
-		HSSD_voidDisplayNumber(DIO_PORTA,ONE);
+		for(j=0; j<3;j++)
+		{
+			HLED_voidTurnLEDOn(DIO_PORTC,LED_Arr[j]);
+
+			if(j==0 || j==2)
+			{
+				HSSD_voidDisplayNumber(DIO_PORTB,SSD_Arr[1]); // 1
+				HSSD_voidDisplayNumber(DIO_PORTA,SSD_Arr[0]); //0
+				_delay_ms(500);
+//
+				HSSD_voidDisplayNumber(DIO_PORTB,SSD_Arr[0]); // 0
+				HSSD_voidDisplayNumberDescending(DIO_PORTA,500);
+//				_delay_ms(1000);
+			}
+
+			else
+			{
+				for(i=3; i>0;i--)
+				{
+					MDIO_voidSetPortValue(DIO_PORTA,(SSD_Arr[i]));
+					_delay_ms(500);
+				}
+			}
+			HLED_voidTurnLEDOff(DIO_PORTC,LED_Arr[j]);
+
+		}
 	}
-//	// Configure PORTA as Output for SSD1
-//	MDIO_voidSetPortDirection(DIO_PORTA,0xFF);
-//
-//	// Configure PORTB as Output for SSD2
-//	MDIO_voidSetPortDirection(DIO_PORTB,0xFF);
-//
-//	//Configure PORTC as Output for LEDs(Green,Yellow ,Red)
-//	MDIO_voidSetPortDirection(DIO_PORTC,0x07);
-//
-//	u8 SSD_Arr[10]={ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE};
-//	u8 LED_Arr[3]={GREEN,YELLOW ,RED};
-//	u8 i=3;
-//	u8 j=0;
-//	while(1)
-//	{
-//		for(j=0; j<3;j++)
-//		{
-//			HLED_voidTurnLEDOn(DIO_PORTC,LED_Arr[j]);
-//			if(j==0 || j==2)
-//			{
-//				HSSD_voidDisplayNumber(DIO_PORTB,SSD_Arr[1]); // 1
-//				HSSD_voidDisplayNumber(DIO_PORTA,SSD_Arr[0]); //0
-//				_delay_ms(1000);
-//
-//				HSSD_voidDisplayNumber(DIO_PORTB,SSD_Arr[0]); // 0
-//				HSSD_voidDisplayNumberDescending(DIO_PORTA,1000);
-//				_delay_ms(1000);
-//
-//
-//			}
-//			else
-//			{
-//				for(i=3; i>0;i--)
-//				{
-//					MDIO_voidSetPortValue(DIO_PORTA,~(SSD_Arr[i]));
-//					_delay_ms(1000);
-//				}
-//			}
-//			HLED_voidTurnLEDOff(DIO_PORTC,LED_Arr[j]);
-//		}
-//
-//	}
-
-
-
 }
+
+
+/****************************/
+
+
+
+
 
 
 
--- TrafficLightSystem/Src/SSD_Program.c
+++ TrafficLightSystem/Src/SSD_Program.c
@@ -14,6 +14,8 @@
 #include "../Include/MCAL/DIO/DIO_Private.h"
 #include "../Include/MCAL/DIO/DIO_Cfg.h"
 
+
+
 #include "../Include/HAL/SSD/SSD_Interface.h"
 
 void HSSD_voidDisplayNumber(DIO_PORT_e A_portID, u8 A_u8NumberValue)
@@ -24,19 +26,19 @@ void HSSD_voidDisplayNumber(DIO_PORT_e A_portID, u8 A_u8NumberValue)
 		switch(A_portID)
 		{
 		case DIO_PORTA:
-			MDIO_voidSetPortDirection(A_portID,~(A_u8NumberValue));
+//			MDIO_voidSetPortDirection(A_portID,~(A_u8NumberValue));
 			MDIO_voidSetPortValue(A_portID,(A_u8NumberValue));
 			break;
 		case DIO_PORTB:
-			MDIO_voidSetPortDirection(A_portID,~(A_u8NumberValue));
+//			MDIO_voidSetPortDirection(A_portID,~(A_u8NumberValue));
 			MDIO_voidSetPortValue(A_portID,(A_u8NumberValue));
 			break;
 		case DIO_PORTC:
-			MDIO_voidSetPortDirection(A_portID,~(A_u8NumberValue));
+//			MDIO_voidSetPortDirection(A_portID,~(A_u8NumberValue));
 			MDIO_voidSetPortValue(A_portID,(A_u8NumberValue));
 			break;
 		case DIO_PORTD:
-			MDIO_voidSetPortDirection(A_portID,~(A_u8NumberValue));
+//			MDIO_voidSetPortDirection(A_portID,~(A_u8NumberValue));
 			MDIO_voidSetPortValue(A_portID,(A_u8NumberValue));
 			break;
 		}
@@ -50,10 +52,10 @@ void HSSD_voidDisplayNumber(DIO_PORT_e A_portID, u8 A_u8NumberValue)
 void HSSD_voidDisplayNumberAscending(DIO_PORT_e A_portID , u16 A_u16TimeToWait)
 {
 	u8 SSD_arr[10]={ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE};
-	for(u8 i=0; i<10;i++)
+	for(u8 i=0; i<=9;i++)
 	{
-		MDIO_voidSetPortDirection(A_portID,~(SSD_arr[i]));
-		MDIO_voidSetPortValue(A_portID,~(SSD_arr[i]));
+//		MDIO_voidSetPortDirection(A_portID,~(SSD_arr[i]));
+		MDIO_voidSetPortValue(A_portID,(SSD_arr[i]));
 		_delay_ms(A_u16TimeToWait);
 	}
 }
@@ -63,8 +65,8 @@ void HSSD_voidDisplayNumberDescending (DIO_PORT_e A_portID , u16 A_u16TimeToWait
 	u8 SSD_arr[10]={ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE};
 	for(u8 i=9; i>=0;i--)
 	{
-		MDIO_voidSetPortDirection(A_portID,~(SSD_arr[i]));
-		MDIO_voidSetPortValue(A_portID,~(SSD_arr[i]));
+//		MDIO_voidSetPortDirection(A_portID,~(SSD_arr[i]));
+		MDIO_voidSetPortValue(A_portID,(SSD_arr[i]));
 		_delay_ms(A_u16TimeToWait);
 	}
 }
